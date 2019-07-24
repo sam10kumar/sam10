fcd0c224b78f66633768363004583b792c1f304a
sam10kumar <52233580+sam10kumar@users.noreply.github.com>
2019-07-24 22:38:23 +0530

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,15 @@
+S=input()
+if(S=='A' or S=='a'):
+	print('Vowel') 
+elif(S=='E' or S=='e'):
+	print('Vowel')
+elif(S=='I' or S=='i'):
+	print('Vowel')
+elif(S=='O' or S=='o' ):
+	print('Vowel')
+elif(S=='U' or S=='u'):
+	print('Vowel')
+elif(S>='a' and S<='z') or (S>='A' and S<='Z'):
+	print("Consonant")
+else:
+	print('invalid')
